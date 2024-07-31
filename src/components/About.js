459f4ab87b804a5ec17b27a272aa269f0b79efcc
kenedy4 <kennedymusyoki8@gmail.com>
2024-07-31 21:42:58 +0300

    Image error sorted

--- src/components/About.js
+++ src/components/About.js
@@ -4,11 +4,11 @@ import { image } from "../data/data";
 function About() {
   return (
     <div id="about">
-      <h2>About Me</h2>;<p>Learning JSX</p>;
-      <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />;
+      <h2>About Me</h2>
+      <p>Learning JSX</p>
+      <img src={image} alt="I made this" />
     </div>
   );
-  // update the JSX being returned!
 }
 
 export default About;
